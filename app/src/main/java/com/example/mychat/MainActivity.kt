c3f68f823ed30339f1b4128f3268b18199edf867
MuhIrsyadddd <irsyadnaxcholy12@gmail.com>
2025-12-12 00:09:28 +0700

    Tes

--- app/src/main/java/com/example/mychat/MainActivity.kt
+++ app/src/main/java/com/example/mychat/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyChatTheme {
-        Greeting("Android")
+        Greeting("Test")
     }
 }
